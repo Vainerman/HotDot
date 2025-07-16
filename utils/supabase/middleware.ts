8098e4fa042be6de2c15bc12f595cfde986f9f7f
Vainerman <34982551+Vainerman@users.noreply.github.com>
2025-07-16 13:26:40 +0300

    refactor: improve cookie handling and response management in middleware
    - Enhanced cookie management in the updateSession function, ensuring proper setting and removal of cookies with updated response handling.
    - Streamlined the response object creation for better clarity and consistency in the middleware logic.
    - Added missing semicolons for improved code style and readability.

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -6,7 +6,7 @@ export async function updateSession(request: NextRequest) {
     request: {
       headers: request.headers,
     },
-  })
+  });
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -14,27 +14,47 @@ export async function updateSession(request: NextRequest) {
     {
       cookies: {
         get(name: string) {
-          return request.cookies.get(name)?.value
+          return request.cookies.get(name)?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
           response.cookies.set({
             name,
             value,
             ...options,
-          })
+          });
         },
         remove(name: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value: '',
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
           response.cookies.set({
             name,
             value: '',
             ...options,
-          })
+          });
         },
       },
-    }
-  )
+    },
+  );
 
-  await supabase.auth.getUser()
+  await supabase.auth.getUser();
 
-  return response
+  return response;
 } 
