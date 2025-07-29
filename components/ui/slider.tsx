4eac321510cc04610863cb15a170a36b9f508d4b
Vainerman <34982551+Vainerman@users.noreply.github.com>
2025-07-29 21:07:46 +0300

    refactor: update slider component for improved pointer event handling
    - Added pointer-events-none to the SliderPrimitive.Track to prevent interaction with the track.
    - Enabled pointer-events-auto on the SliderPrimitive.Thumb to allow user interaction with the thumb while maintaining track non-interactivity.

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -17,11 +17,11 @@ const Slider = React.forwardRef<
     )}
     {...props}
   >
-    <SliderPrimitive.Track className="relative h-auto w-full grow overflow-visible bg-transparent">
+    <SliderPrimitive.Track className="relative h-auto w-full grow overflow-visible bg-transparent pointer-events-none">
       <div className="h-[15px] w-full bg-slider-track bg-no-repeat bg-center" />
       <SliderPrimitive.Range className="absolute h-full bg-transparent" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-[90px] w-4 bg-slider-thumb bg-no-repeat bg-center cursor-pointer ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-default disabled:opacity-50" />
+    <SliderPrimitive.Thumb className="block h-[90px] w-4 bg-slider-thumb bg-no-repeat bg-center cursor-pointer ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-default disabled:opacity-50 pointer-events-auto" />
   </SliderPrimitive.Root>
 ))
 Slider.displayName = SliderPrimitive.Root.displayName
